979b33af6741b0e7cb1fc69fdcc8b175a308a21a
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-06-07 15:42:17 +0100

    test(fetchWithTimeout)

---
+++ src/helpers/fetchWithTimeout/index.test.js
@@ -0,0 +1,39 @@
+import fetchWithTimeout from './index'
+
+describe('fetchWithTimeout', () => {
+    
+    beforeEach(() => {
+        jest.resetAllMocks()
+    })
+
+    it('should throw error on timeout', async () => {
+        // Arrange
+        const fetchPromise = new Promise((resolve, _) => {
+            setTimeout(() => {
+                resolve()
+            }, 100)
+        })
+
+        global.fetch = jest.fn().mockReturnValue(fetchPromise)
+
+        // Act & Assert
+        await (expect(fetchWithTimeout('test',{}, 10)).rejects.toThrow('Timeout'))
+    })
+
+    it('should return result', async () => {
+        // Arrange
+        const mockResponse = { foo: 'bar '}
+        const fetchPromise = new Promise((resolve, _) => {
+            setTimeout(() => {
+                resolve(mockResponse)
+            }, 100)
+        })
+        global.fetch = jest.fn().mockReturnValue(fetchPromise)
+
+        // Act
+        const result = await fetchWithTimeout('test')
+
+        // Assert
+        expect(result).toEqual(mockResponse)
+    })
+})
