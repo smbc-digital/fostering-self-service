f85bfdb015e52c1f837373fe3d94796f8bc28651
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-06-07 15:41:50 +0100

    test(Provider): Add Provider tests for the new functionality

--- src/components/Provider/index.test.js
+++ src/components/Provider/index.test.js
@@ -1,33 +1,120 @@
 import { React, mount } from '../../helpers/SetupTest'
 import Provider from '../Provider'
-import { Context } from '../../context'
+import { Context } from '../../context/'
+import * as helpers from '../../helpers'
 
-describe('Provider', () => {
-    const setStateMock = jest.fn()
-    const useStateMock = jest.fn()
-    let wrapper
-    
-    beforeEach(() => {
-        React.useState = useStateMock
-        
-        useStateMock.mockReturnValue([{}, setStateMock])
-        wrapper = mount(<Provider/>)
+const TestComponent = ({ context: { onChange, displayRecaptcha, firstName, lastName } }) => {
+
+    const onClick = () => {
+		onChange(
+			{
+				target: {
+					name: 'firstName',
+					value: 'new value'
+				}
+			},
+			true
+		)
+	}
+
+    return (
+		<div>
+			<button
+				id="test-button"
+				onClick={onClick}
+			>
+				Test button
+			</button>
+			{displayRecaptcha ? <p className="recaptcha" /> : <p className="no-recaptcha" />}
+			<p className="name">{firstName.value}</p>
+			<p className="last-name">{lastName.value}</p>
+		</div>
+	)
+}
+
+const mountProviderWithCase = (children) => {
+    const mockCaseResponse = {
+        statuses: {
+            status: 0
+        },
+        firstName: 'test',
+        lastName: null
+    }
+
+    const responseMock = { json: jest.fn().mockReturnValue(mockCaseResponse) }
+    const fetchPromise = Promise.resolve(responseMock)
+    helpers.fetchWithTimeout = jest.fn().mockReturnValue(fetchPromise)
+    const wrapper = mount(<Provider>{children}</Provider>)
+
+    const mountPromise = fetchPromise.then(() => {
+        wrapper.setProps()
+    }).then(() => {
+        wrapper.setProps()
     })
 
-    it('should create the base state with objects', () => {
-        //Assert
-        expect(wrapper.find(Context.Provider).exists).toBeTruthy()
+    return [wrapper, mountPromise]
+}
+
+describe('Provider', () => {
+
+    beforeEach(() => {
+        jest.resetAllMocks()
+    })
+
+    it('should render loading state', () => {
+        // Arrange
+        const wrapper = mount(<Provider/>)
+
+        // Assert
+        expect(wrapper.find('p').text()).toEqual('Loading...')
+    })
+
+    it('should render error state', () => {
+        // Arrange
+        const fetchPromise = Promise.reject(new Error())
+        const wrapper = mount(<Provider/>)
+        helpers.fetchWithTimeout = jest.fn().mockReturnValue(fetchPromise)
+
+        // Act & Assert
+        return fetchPromise.catch(() => {
+            wrapper.update()
+        }).then(() => {
+            expect(wrapper.find('p').text()).toEqual('Error')
+        })
+    })
+
+    it('should render itself', () => {
+        // Act & Assert
+        const [wrapper, mountPromise] = mountProviderWithCase()
+        
+        return mountPromise.then(() => {
+            expect(wrapper.find('Provider').exists()).toBe(true)
+        })
+    })
+
+    it('should map case to context', () => {
+        // Arrange
+        const [wrapper, mountPromise] = mountProviderWithCase(<Context.Consumer>{context => <TestComponent context={context}/>}</Context.Consumer>)
+
+        return mountPromise.then(() => {
+            expect(wrapper.find('.name').text()).toEqual('test')
+            expect(wrapper.find('.last-name').text()).toEqual('')
+        })
     })
 
     describe('reCaptcha', () => {
         it('should set display reCaptcha correctly when no div exists', () => {
-            const state = useStateMock.mock.calls[0][0]
+            // Act
+            const [wrapper, mountPromise] = mountProviderWithCase(<Context.Consumer>{context => <TestComponent context={context}/>}</Context.Consumer>)
 
             //Assert
-            expect(state.displayRecaptcha).toBe(false)
+            return mountPromise.then(() => {
+                expect(wrapper.find('.no-recaptcha').exists()).toBe(true)
+            })
         })
 
         it('should set display reCaptcha correctly when div exists and value is true', () => {
+            // Arrange
             document.getElementById = jest.fn()
             document.getElementById.mockReturnValue(
                 {
@@ -35,15 +122,17 @@ describe('Provider', () => {
                 }
             )
             
-            //Act
-            mount(<Provider/>)
-            const state =  useStateMock.mock.calls[useStateMock.mock.calls.length-1][0]
-
-            //Assert
-            expect(state.displayRecaptcha).toBe(true)
+            // Act
+            const [wrapper, mountPromise] = mountProviderWithCase(<Context.Consumer>{context => <TestComponent context={context}/>}</Context.Consumer>)
+            
+            // Assert
+            return mountPromise.then(() => {
+                expect(wrapper.find('.recaptcha').exists()).toBe(true)
+            })
         })
 
         it('should set display reCaptcha correctly when div exists and value is false', () => {
+            // Arrange
             document.getElementById = jest.fn()
             document.getElementById.mockReturnValue(
                 {
@@ -51,34 +140,27 @@ describe('Provider', () => {
                 }
             )
             
-            //Act
-            mount(<Provider/>)
-            const state =  useStateMock.mock.calls[1][0]
-
-            //Assert
-            expect(state.displayRecaptcha).toBe(false)
+            // Act
+            const [wrapper, mountPromise] = mountProviderWithCase(<Context.Consumer>{context => <TestComponent context={context}/>}</Context.Consumer>)
+            
+            // Assert
+            return mountPromise.then(() => {
+                expect(wrapper.find('.no-recaptcha').exists()).toBe(true)
+            })
         })
     })
 
     describe('onChange', () => {
-        it('should call setState', () => {
-            //Arrange 
-            const onChange = useStateMock.mock.calls[0][0].onChange
+        it('should update state', () => {
+            // Arrange
+            const [wrapper, mountPromise] = mountProviderWithCase(<Context.Consumer>{context => <TestComponent context={context}/>}</Context.Consumer>)
 
-            //Act
-            onChange({
-                    target: {
-                        name: 'test',
-                        value: 'some test value'
-                    }
-                }, true)
-
-            //Assert
-            expect(setStateMock).toHaveBeenCalledWith({
-                test: {
-                    value: 'some test value',
-                    isValid: true
-                }
+            return mountPromise.then(() => {
+                expect(wrapper.find('.name').text()).toEqual('test')
+                wrapper.find('#test-button').simulate('click')
+                wrapper.update()
+            }).then(() => {
+                expect(wrapper.find('.name').text()).toEqual('new value')
             })
         })
     })
