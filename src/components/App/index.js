9e393c9cb1b799b7979e20feab28a7abc7fb029e
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-06-14 15:20:06 +0100

    refactor: Update routes in App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,11 +3,12 @@ import { Switch, Route } from 'react-router'
 import { getPageRoute } from '../../helpers/pagehelper'
 import { ErrorPage } from 'smbc-react-components'
 import Start from '../Pages/Start'
-import { KnownByAnotherName } from '../Pages/1-TellUsAboutYou'
+import { KnownByAnotherName, MoreAboutYou } from '../Pages/1-TellUsAboutYou'
 
 const App = () => <Switch>
     <Route exact path={getPageRoute(1)} component={Start} />
-    <Route exact path={getPageRoute(2)} component={KnownByAnotherName} />
+    <Route exact path={`${getPageRoute(2)}/(|second-applicant)?`} component={KnownByAnotherName} />
+    <Route exact path={`${getPageRoute(3)}/(|second-applicant)?`} component={MoreAboutYou} />
     <Route exact path="/error" component={ErrorPage} />
 </Switch>
 
