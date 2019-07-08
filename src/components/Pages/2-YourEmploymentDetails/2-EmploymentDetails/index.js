ab434d2433134df7336fb28c076f0ca4b0bfabbb
Jordan <jordan.burns@stockport.gov.uk>
2019-07-08 11:42:25 +0100

    chore(EmploymentDetails): Change names from p to h3

--- src/components/Pages/2-YourEmploymentDetails/2-EmploymentDetails/index.js
+++ src/components/Pages/2-YourEmploymentDetails/2-EmploymentDetails/index.js
@@ -69,7 +69,7 @@ const EmploymentDetails = ({ history, match }) => {
     return <form onSubmit={onSubmit}>
                 <h1>Your fostering journey</h1>
                 <h2>Your employment details</h2>
-                <p>{firstName.value} {lastName.value}</p>
+                <h3>{firstName.value} {lastName.value}</h3>
                 <TextInputContainer
                     label='Current employer'
                     id='currentEmployer'
