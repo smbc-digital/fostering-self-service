d2a7b861150531a8e42135cdee1e6e7c3006811f
Jordan <jordan.burns@stockport.gov.uk>
2019-07-09 16:16:40 +0100

    fix(App): Amend page routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -36,10 +36,9 @@ const App = () => (
 		<Route exact path={`${getPageRoute(4)}/(|second-applicant)?`} component={AreYouEmployed} />
 		<Route exact path={`${getPageRoute(5)}/(|second-applicant)?`} component={EmploymentDetails} />
 		<Route exact path={getPageRoute(6)} component={LanguagesSpokenInYourHome} />
-		<Route exact path={`${getPageRoute(7)}/(|second-applicant)?`} component={YourHealth} />
-		<JointApplicationOnlyRoute exact path={getPageRoute(8)} component={AreYouMarried} />
-		<JointApplicationOnlyRoute exact path={getPageRoute(9)} component={MarriageDate} />
-		<JointApplicationOnlyRoute exact path={getPageRoute(10)} component={MovedInTogetherDate} />
+		<JointApplicationOnlyRoute exact path={getPageRoute(7)} component={AreYouMarried} />
+		<JointApplicationOnlyRoute exact path={getPageRoute(8)} component={MarriageDate} />
+		<JointApplicationOnlyRoute exact path={getPageRoute(9)} component={MovedInTogetherDate} />
 		<Route exact path={`${getPageRoute(10)}/(|second-applicant)?`} component={HaveYouPreviouslyApplied} />
 		<Route exact path={`${getPageRoute(11)}/(|second-applicant)?`} component={YourHealth} />
 		<Route exact path={getPageRoute(12)} component={AboutYourInterest} />
