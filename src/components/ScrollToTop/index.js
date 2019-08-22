701debe3b0c2074620fa061e3af339ec7d47da3b
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-08-22 10:54:11 +0100

    fix(Scroll): Fix scrolling to additional information section

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -2,8 +2,12 @@ import { useEffect } from 'react'
 import { withRouter } from 'react-router'
 import PropTypes from 'prop-types'
 
-export const ScrollToTop = ({ location, children }) => {
+export const ScrollToTop = ({ location, children, ignoredPaths }) => {
 	useEffect(() => {
+		if (ignoredPaths.indexOf(location.pathname) !== -1) {
+			return
+		}
+
 		window.scrollTo(0, 0)
 	}, [location])
 
@@ -13,6 +17,11 @@ export const ScrollToTop = ({ location, children }) => {
 ScrollToTop.propTypes = {
 	location: PropTypes.object,
 	children: PropTypes.object,
+	ignoredPaths: PropTypes.array
+}
+
+ScrollToTop.defaultProps = {
+	ignoredPaths: []
 }
 
 export default withRouter(ScrollToTop)
